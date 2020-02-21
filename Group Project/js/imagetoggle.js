df157becea7aed24655863a505d0e001135ea1ff
Shane <shanemanninglee@gmail.com>
2020-02-20 23:06:48 -0500

    updated modal

--- Group Project/js/imagetoggle.js
+++ Group Project/js/imagetoggle.js
@@ -1,15 +1,27 @@
-let element = document.getElementById("photo");
+// let element = document.getElementById("photo");
 
-function addClass() {
-  element.classList.add("enlarge");
-} 
+// function addClass() {
+//   element.classList.add("enlarge");
+// } 
 
-function toggleClass(){
-  if (element.classList.contains("enlarge")) {
-    element.classList.remove("enlarge")
-  } else {
-    element.classList.add("enlarge")  
-  }
+// function toggleClass(){
+//   if (element.classList.contains("enlarge")) {
+//     element.classList.remove("enlarge")
+//   } else {
+//     element.classList.add("enlarge")  
+//   }
+// }
+
+// element.addEventListener("click", toggleClass);
+
+var image = document.getElementsByClassName("modalImage");
+
+function openModal(n){
+  document.getElementById("imgContainer").style.display = "block";
+  image[n].style.display = "block";
 }
 
-element.addEventListener("click", toggleClass);
+function closeModal(n){
+  document.getElementById("imgContainer").style.display = "none";
+  image[n].style.display = "none";
+}
